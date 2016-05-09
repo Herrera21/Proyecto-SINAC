ae40e162b8ef284b19d0f5b314111bf8e8215c92
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2016-05-09 01:14:55 -0600

    javascript de calendario

---
+++ Sistema/Sistema/CapaPresentacion/Js/FechasEstandar.js
@@ -0,0 +1,22 @@
+﻿function Configurafecha(maxDate, id, rangoFecha)
+{
+    if(maxDate)
+    {
+        $(id).datepicker({
+            inline: true,
+            changeMonth: true,
+            changeYear: true,
+            yearRange: rangoFecha,
+            maxDate: "+0m +0d"
+        });
+    } else
+    {
+        $(id).datepicker({
+            inline: true,
+            changeMonth: true,
+            changeYear: true,
+            yearRange: rangoFecha,
+        });
+    }
+
+}
