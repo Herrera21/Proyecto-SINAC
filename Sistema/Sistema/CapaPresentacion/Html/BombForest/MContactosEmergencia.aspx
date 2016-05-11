e2cba0470a5b92ef822398ddbb25a54a617f04a4
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2016-05-11 01:49:27 -0600

    contacto emergencia

---
+++ Sistema/Sistema/CapaPresentacion/Html/BombForest/MContactosEmergencia.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/CapaPresentacion/Html/PaginaEstandar.Master" AutoEventWireup="true" CodeBehind="MContactosEmergencia.aspx.cs" Inherits="Sistema.CapaPresentacion.Html.BombForest.MContactosEmergencia" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolderContenido" runat="server">
+    <script>
+        function idBoton() {
+            return "<%=Button1.ClientID%>";
+        }
+    </script>
+    <!--mensaje-->
+    <div id="mensajes" class="modal" data-backdrop="static" data-keyboard="false" style="display: none; ">
+            <div class="modal-header">
+              <h3>Mensaje</h3>
+            </div>
+            <div class="modal-body">
+               <label id="labelMensaje" runat="server"></label>		        
+            </div>
+            <div class="modal-footer">
+              <a id="botonMensaje1" runat="server" href="CContactosEmergencia.aspx" class="btn">Cerrar</a>
+              <a id="botonMensaje2" runat="server" href="#" class="btn" data-dismiss="modal">Cerrar</a>
+            </div>
+    </div>
+
+    <!--contenido-->
+    <div class="container">
+
+        <!-- Miga -->
+            <div class="row" style="background-color:white; width:99.7%">
+                <ol class="breadcrumb">
+                    <li><a href="../MenuPrincipal.aspx"> Menú Principal </a></li>
+                    <li><a href="../CAreaConserv.aspx">Áreas de conservación </a></li>
+                    <li><a href="../CBrigadas.aspx">Brigada </a></li>
+                    <li><a href="../CBomberos.aspx">Bomberos </a></li>
+                    <li class="active">Información detallada </li>
+                    <li class="active">Equipo de protección personal </li>
+                    <li class="active">Modificar </li>
+                </ol>
+            </div>
+
+            <div class="row" style="background-color:white; width:99.7%">
+                <div class="col-sm-6 col-md-4 col-md-offset-4">
+                    <h1 class="text-center login-title">Modificar Área de Conservación</h1>
+                    <div class="account-wall">
+                        <form id="form1" class="form-signin" runat="server">
+                        <!--alert-->
+                        <div id="alert" class="modal" data-backdrop="static" data-keyboard="false" style="display: none; ">
+                                <div class="modal-header">
+                                  <h3>Alerta</h3>
+                                </div>
+                                <div class="modal-body">
+                                   <label id="labelAlert" runat="server"></label>		        
+                                </div>
+                                <div class="modal-footer">
+                                  <asp:Button ID="ButtonAceptar" runat="server" Text="Aceptar" class="btn" OnClick="ButtonAcepta"/>
+                                  <asp:Button ID="ButtonCancelar" runat="server" Text="Cancelar" class="btn" OnClick="ButtonCancela"/>
+                                </div>
+                        </div>
+                          <!--formulario-->
+                          <div class="form-group">
+                            <label>Nombre</label>
+                            <input type="text" class="form-control" id="nombre" runat="server"
+                                placeholder="Ingrese el nombre" required autofocus/>
+                          </div>
+                          <div class="form-group">
+                            <label>Primer apellido</label>
+                            <input type="text" class="form-control" id="p_Ape" runat="server"
+                                   placeholder="Ingrese el primer apellido"/>
+                          </div>
+                          <div class="form-group">
+                            <label>Segundo apellido</label>
+                            <input type="text" class="form-control" id="s_Ape" runat="server"
+                                   placeholder="Ingrese el segundo apellido"/>
+                          </div>
+                          <div class="form-group">
+                            <label>Cédula</label>
+                            <input type="text" class="form-control" id="cedula" runat="server" 
+                                   placeholder="Ingrese la cédula"/>
+                          </div>
+                          <div class="form-group">
+                            <label>Parentesco</label>
+                            <input type="text" class="form-control" id="parent" runat="server" 
+                                   placeholder="Ingrese el segundo apellido"/>
+                          </div>
+                          <div class="form-group">
+                            <label>Teléfono celular</label>
+                            <input type="text" class="form-control" id="telCel" runat="server"
+                                placeholder="Ingresar el teléfono celular"/>
+                          </div>
+                          <div class="form-group">
+                            <label>Teléfono casa</label>
+                            <input type="text" class="form-control" id="telCasa" runat="server"
+                                   placeholder="Ingresar el teléfono de casa"/>
+                          </div>
+
+                          <asp:Button ID="Button1" class="btn btn-primary" runat="server" Text="Guardar" Enabled="true" OnClick="Button1_Click" OnClientClick="area()" />
+                          <asp:Button ID="Button2" class="btn btn-primary" runat="server" Text="Inactivar" OnClick="Button2_Click" OnClientClick="area()" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+</asp:Content>
---
+++ Sistema/Sistema/CapaPresentacion/Html/BombForest/MContactosEmergencia.aspx.cs
@@ -0,0 +1,165 @@
+﻿using CapaConfiguracion;
+using CapaLogica;
+using Sistema.CapaLogicaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sistema.CapaPresentacion.Html.BombForest
+{
+    public partial class MContactosEmergencia : System.Web.UI.Page
+    {
+        private const string buttonName = "Inactivar";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (VariablesSeccionControl.Lee<string>("userAreaConserv") == null)
+                {
+                    Response.Redirect("/index.aspx");
+                }
+                else
+                {
+                    if (VariablesSeccionControl.Lee<string>("Bombero") == null)
+                    {
+                        Response.Redirect("CAreaConserv.aspx");
+                    }
+                    else
+                    {
+                        buscarCE(VariablesSeccionControl.Lee<string>("ContEmer"));
+                    }
+                }
+            }
+        }
+
+        protected void activaModal(string id, bool activar)
+        {
+            if (activar)
+            {
+                string script = string.Format("javascript:$('#" + id + "').modal('show');");
+                ScriptManager.RegisterStartupScript(this, Page.ClientScript.GetType(), null, script, true);
+            }
+            else
+            {
+                string script = string.Format("javascript:$('#" + id + "').modal('hidden');");
+                ScriptManager.RegisterStartupScript(this, Page.ClientScript.GetType(), null, script, true);
+            }
+        }
+        protected void mensaje(String mensaje, Boolean redireccionar)
+        {
+            this.labelMensaje.InnerText = mensaje;
+            if (redireccionar)
+            {
+                botonMensaje1.Visible = true;
+                botonMensaje2.Visible = false;
+            }
+            else
+            {
+                botonMensaje1.Visible = false;
+                botonMensaje2.Visible = true;
+            }
+            activaModal("mensajes", true);
+        }
+
+        protected void Alert(String mensaje)
+        {
+            this.labelAlert.InnerText = mensaje;
+            activaModal("alert", true);
+        }
+
+        protected void ButtonAcepta(object sender, EventArgs e)
+        {
+            //activa o inactiva area de conservacion
+            ContactosEmergenciaDB DB = new ContactosEmergenciaDB();
+
+            if (Button2.Text.Equals(buttonName))
+            {
+                if (DB.inactivar(true, VariablesSeccionControl.Lee<string>("ContEmer")))
+                {
+                    mensaje("El contacto de emergencia se ha inactivado", true);
+                }
+                else
+                {
+                    mensaje("Ocurrio un error al guardar la información", false);
+                }
+            }
+            else
+            {
+                if (DB.inactivar(false, VariablesSeccionControl.Lee<string>("ContEmer")))
+                {
+                    mensaje("El contacto de emergencia se ha activado", true);
+                }
+                else
+                {
+                    mensaje("Ocurrio un error al guardar la información", false);
+                }
+            }
+        }
+
+        protected void ButtonCancela(object sender, EventArgs e)
+        {
+            activaModal("alert", false);
+        }
+
+        protected void cargarInfo(string identificacion, string nombre, string apellido1, string apellido2, string parentesco, string telCelular, string telCasa)
+        {
+            this.cedula.Value = identificacion;
+            this.nombre.Value = nombre;
+            this.p_Ape.Value = apellido1;
+            this.s_Ape.Value = apellido2;
+            this.parent.Value = parentesco;
+            this.telCel.Value = telCelular;
+            this.telCasa.Value = telCasa;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (Button2.Text.Equals(buttonName))
+            {
+                Alert("¿Desea realmente desactivar este contacto de emergencia?");
+            }
+            else
+            {
+                Alert("¿Desea realmente reactivar este contacto de emergencia?");
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //guarda la informacion en la base de datos
+            ContactosEmergenciaDB DB = new ContactosEmergenciaDB();
+            if (DB.actualizar(VariablesSeccionControl.Lee<string>("ContEmer"), new ContEmerg(cedula.Value, nombre.Value, p_Ape.Value, s_Ape.Value, parent.Value, telCel.Value, telCasa.Value, null)))
+            {
+                mensaje("El contacto de emergencia ha sido modificado", true);
+            }
+            else
+            {
+                mensaje("Ocurrio un error al guardar la información", false);
+            }
+        }
+
+        private void buscarCE(string id)
+        {
+            //carga la informacion de la base de datos
+            ContactosEmergenciaDB DB = new ContactosEmergenciaDB();
+            ContEmerg temp = DB.seleccionar(id);
+
+            if (temp != null)
+            {
+                if (!DB.getEstado(VariablesSeccionControl.Lee<string>("ContEmer")))
+                {
+                    Button2.Text = "Activar";
+                }
+
+                cargarInfo(temp.getIdentificacion(), temp.getNombre(), temp.getApellido1(), temp.getApellido2(), temp.getParentesco(), temp.getTelCelular(), temp.getTelCasa());
+            }
+            else
+            {
+                mensaje("Ocurrio un error al cargar la información", true);
+            }
+        }
+    }
+}
