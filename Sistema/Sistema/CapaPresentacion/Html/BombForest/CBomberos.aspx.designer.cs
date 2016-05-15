9be345a69f570ec45c71159bf87d0d5ab0ce5442
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2016-05-15 02:30:35 -0600

    restriccion de area conserv para coordinador regional

--- Sistema/Sistema/CapaPresentacion/Html/BombForest/CBomberos.aspx.designer.cs
+++ Sistema/Sistema/CapaPresentacion/Html/BombForest/CBomberos.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace Sistema.CapaPresentacion.Html {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// labelArea control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl labelArea;
+        
+        /// <summary>
         /// Area control.
         /// </summary>
         /// <remarks>
