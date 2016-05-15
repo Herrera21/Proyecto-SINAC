fe8690ba79851bac67352c2f0db2ddf85648c85d
WillyMendez <will.mendez@hotmail.com>
2016-05-15 02:56:44 -0600

    carnet

--- Sistema/Sistema/CapaLogicaNegocio/Carnet.cs
+++ Sistema/Sistema/CapaLogicaNegocio/Carnet.cs
@@ -10,14 +10,16 @@ namespace Sistema.CapaLogicaNegocio
         private int anioCarnet;
         private string emisionCarnet;
         private string fechaVencimiento;
+        private string id_Bombero;
 
         public Carnet() { }
 
-        public Carnet(int anioCarnet, string emisionCarnet, string fechaVencimiento)
+        public Carnet(int anioCarnet, string emisionCarnet, string fechaVencimiento, string id_Bombero)
         {
             this.anioCarnet = anioCarnet;
             this.emisionCarnet = emisionCarnet;
             this.fechaVencimiento = fechaVencimiento;
+            this.id_Bombero = id_Bombero;
         }
 
         public void setAnioCarnet(int anioCarnet)
@@ -48,6 +50,16 @@ namespace Sistema.CapaLogicaNegocio
         public string getFechaVencimiento()
         {
             return fechaVencimiento;
+        }
+
+        public void setId_Bombero(string id_Bombero)
+        {
+            this.id_Bombero = id_Bombero;
+        }
+
+        public string getId_Bombero()
+        {
+            return id_Bombero;
         }
     }
 }
