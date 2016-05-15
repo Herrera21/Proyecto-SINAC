8d77890aebc26d2126fffb5a6fc5d1cded02a64f
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2016-05-15 01:25:46 -0600

    Reseña Médica

--- Sistema/Sistema/CapaConfiguracion/ReseniaMedicaDB.cs
+++ Sistema/Sistema/CapaConfiguracion/ReseniaMedicaDB.cs
@@ -15,10 +15,6 @@ namespace Sistema.CapaConfiguracion
     {
         public ReseniaMedicaDB() { }
 
-        public ReseniaMedicaDB(byte v1, string text1, byte v2, string text2, byte v3, string text3, byte v4, string text4, byte v5, string text5, byte v6, string text6, string text7, string v7)
-        {
-        }
-
         public bool insertar(ReseniaMedic objeto)
         {
             try
@@ -28,7 +24,8 @@ namespace Sistema.CapaConfiguracion
                     return false;
                 }
 
-                SqlCommand ing = new SqlCommand("insert into TB_ReseniaMedica(PK_FK_Id_BomberoForestal, internado, motivoIN, tratamientoMedico, motivoTM, lenteContacto, motivoLC, operado, motivoOP, limitacionFisica, tipoLimitFisic, checkMedic, diagnostico, tipoSangre) values (@id, @internado, @nombre, @motivoIN, @tratamientoMedico, @motivoTM, @lenteContacto, @motivoLC, @operado, @motivoOP, @limitacionFisica, @tipoLimitFisic, @checkMedic, @diagnostico, @tipoSangre)", coneccion);
+                SqlCommand ing = new SqlCommand("insert into TB_ReseniaMedica(PK_FK_Id_BomberoForestal, internado, motivoIN, tratamientoMedico, motivoTM, lenteContacto, motivoLC, operado, motivoOP, limitacionFisica, tipoLimitFisic, checkMedic, diagnostico, tipoSangre) values (@id_Bombero, @internado, @motivoIN, @tratamientoMedico, @motivoTM, @lenteContacto, @motivoLC, @operado, @motivoOP, @limitacionFisica, @tipoLimitFisic, @checkMedic, @diagnostico, @tipoSangre)", coneccion);
+                ing.Parameters.AddWithValue("id_Bombero", objeto.getId_Bombero());
                 ing.Parameters.AddWithValue("internado", objeto.getInternado());
                 ing.Parameters.AddWithValue("motivoIN", objeto.getMotivoIN());
                 ing.Parameters.AddWithValue("tratamientoMedico", objeto.getTratMedic());
@@ -55,6 +52,67 @@ namespace Sistema.CapaConfiguracion
             }
         }
 
-         
+        public bool actualizar(string id, ReseniaMedic objeto)
+        {
+            if (!conectar())
+            {
+                return false;
+            }
+
+            try
+            {
+                SqlCommand ing = new SqlCommand("update TB_ReseniaMedica set internado = @Ointernado, motivoIN = @OmotivoIN, tratamientoMedico = @OtratamientoMedico, motivoTM = @OmotivoTM, lenteContacto = @OlenteContacto, motivoLC = @OmotivoLC, operado = @Ooperado, motivoOP = @OmotivoOP, limitacionFisica = @OlimitacionFisica, tipoLimitFisic = @OtipoLimitFisic, checkMedic = @OcheckMedic, diagnostico = @Odiagnostico, tipoSangre = @OtipoSangre where PK_FK_Id_BomberoForestal = @id", coneccion);
+                ing.Parameters.AddWithValue("Ointernado", objeto.getInternado());
+                ing.Parameters.AddWithValue("OmotivoIN", objeto.getMotivoIN());
+                ing.Parameters.AddWithValue("OtratamientoMedico", objeto.getTratMedic());
+                ing.Parameters.AddWithValue("OmotivoTM", objeto.getMotivoTM());
+                ing.Parameters.AddWithValue("OlenteContacto", objeto.getLentCont());
+                ing.Parameters.AddWithValue("OmotivoLC", objeto.getMotivoLC());
+                ing.Parameters.AddWithValue("Ooperado", objeto.getOperado());
+                ing.Parameters.AddWithValue("OmotivoOP", objeto.getMotivoOP());
+                ing.Parameters.AddWithValue("OlimitacionFisica", objeto.getLimitFisic());
+                ing.Parameters.AddWithValue("OtipoLimitFisic", objeto.getLimitFisic());
+                ing.Parameters.AddWithValue("OcheckMedic", objeto.getCheckMedic());
+                ing.Parameters.AddWithValue("Odiagnostico", objeto.getDiagnostico());
+                ing.Parameters.AddWithValue("OtipoSangre", objeto.getTipoSangre());
+
+                ing.Parameters.AddWithValue("id", id);
+
+                coneccion.Open();
+                ing.ExecuteNonQuery();
+                coneccion.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public ReseniaMedic seleccionar(string id)
+        {
+            if (!conectar())
+            {
+                return null;
+            }
+
+            try
+            {
+                SqlCommand ing = new SqlCommand("select internado, motivoIN, tratamientoMedico, motivoTM, lenteContacto, motivoLC, operado, motivoOP, limitacionFisica, tipoLimitFisic, checkMedic, diagnostico, tipoSangre, PK_FK_Id_BomberoForestal from TB_ReseniaMedica where PK_FK_Id_BomberoForestal = @id", coneccion);
+                ing.Parameters.AddWithValue("id", id);
+
+                coneccion.Open();
+                SqlDataReader objReader = ing.ExecuteReader();
+                objReader.Read();
+                ReseniaMedic temp = new ReseniaMedic(objReader.GetBoolean(0), objReader.GetString(1), objReader.GetBoolean(2), objReader.GetString(3), objReader.GetBoolean(4), objReader.GetString(5), objReader.GetBoolean(6), objReader.GetString(7), objReader.GetBoolean(8), objReader.GetString(9), objReader.GetBoolean(10), objReader.GetString(11), objReader.GetString(12), objReader.GetString(13));
+                coneccion.Close();
+
+                return temp;
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
