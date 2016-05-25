31dba5e3551fba9b714953128479acb14949ae3c
Herrera21 <aleherrera93@hotmail.com>
2016-05-25 11:30:36 -0600

    ProvinciaDB

---
+++ Sistema/Sistema/CapaConfiguracion/ProvinciaDB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaConfiguracion
+{
+    public class ProvinciaDB : ControlBD
+    {
+        public List<string> listaProvincias()
+        {
+            if (!conectar())
+            {
+                return null;
+            }
+
+            try
+            {
+                SqlCommand ing = new SqlCommand("select nombre from TB_Provincia", coneccion);
+
+                coneccion.Open();
+                SqlDataReader objReader = ing.ExecuteReader();
+                List<string> temp = new List<string>();
+
+                while (objReader.Read())
+                {
+                    temp.Add(objReader.GetString(0));
+                }
+
+                coneccion.Close();
+
+                return temp;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
