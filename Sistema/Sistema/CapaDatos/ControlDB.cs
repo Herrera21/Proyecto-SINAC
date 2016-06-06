b9444d6ea9f0e67f3c3e6d22d8b635b2be7feb1f
santiagoSSM <santiagosanchezmadrigal1@hotmail.com>
2016-06-05 23:32:37 -0600

    coneccion con configuracion del sinac

--- Sistema/Sistema/CapaDatos/ControlDB.cs
+++ Sistema/Sistema/CapaDatos/ControlDB.cs
@@ -20,6 +20,7 @@ namespace CapaDatos
             try
             {
                 coneccion = new SqlConnection("Data Source=.; Initial Catalog=BD_SINAC;Integrated Security=true");
+                //coneccion = new SqlConnection("Data Source=172.31.30.16; Initial Catalog=BD_SINAC; User Id=sa; Password=SistemaSNBFEH_SQL_235711;");
                 return true;
             }
             catch
